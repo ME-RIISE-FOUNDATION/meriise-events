97838dc022c5f1ae0ca7c9cc9a1a41adcb25a4d8
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2026-02-07 13:27:03 +0530

    changes

--- src/pages/Event26.jsx
+++ src/pages/Event26.jsx
@@ -13,25 +13,6 @@ function Event26() {
           description="Students gained knowledge on entrepreneurship and were motivated towards entrepreneurial career."
           date="30th April, 2018 "
         />
-        <Carousel
-          images={["/assets/events18/lsc.jpg"]}
-          title="MCE Learnathon and Super coders"
-          description="Students were able to solve industry oriented problems"
-          date="1st and 2nd July 2018"
-        />
-        <Carousel
-          images={["/assets/events18/eepd.jpg"]}
-          title="Visit to Entrepreneurial Establishments and Panel Discussion "
-          description="Students were able to identify their prospective areas of work and gained knowledge on industry practices and regulations."
-          date="17th  September 2018"
-        />
-
-          <Carousel
-          images={["/assets/events18/ideation.jpg"]}
-          title="Ideation"
-          description="Able to create presentations on innovative project ideas that articulate and apply financial, operational, organizational, market, and sales knowledge to identify paths to value creation. "
-          date="  13th  October 2018"
-        />
       </div>
     </>
   )
